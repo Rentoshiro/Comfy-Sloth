505a065899ff1cf64f46c8be3915beb65e287604
Rentoshiro <149387302+Rentoshiro@users.noreply.github.com>
2024-04-23 20:43:54 +0200

    added 'add to cart logic'

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -7,6 +7,16 @@ import {
   CLEAR_CART,
   COUNT_CART_TOTALS,
 } from "../actions";
+import { json } from "react-router-dom";
+
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
 
 const initialState = {
   cart: [],
@@ -19,8 +29,28 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  //add to cart
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
+  //remove item
+  const removeItem = (id) => {};
+  //toggle amount
+  const toggleAmount = (id, value) => {};
+  // clear cart
+  const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
-    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
   );
 };
 
